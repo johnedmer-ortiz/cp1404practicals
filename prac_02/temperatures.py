dd8bc8adc69afdac6f286f9d572271756f7b5bf0
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-19 14:55:56 +1000

    Refactored temperatures.py from prac01 to make temperature conversions into functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,27 +1,31 @@
-"""
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
-"""
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        farenheit = float(input("Farenheit: "))
-        celsius = 5 / 9.0 * (farenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+
+def celsius_to_fahrenheit(celsius):
+    """Converts celsius to fahrenheit"""
+    return celsius * 9.0 / 5 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Converts fahrenheit to celsius"""
+    return 5 / 9.0 * (fahrenheit - 32)
+
+def main():
+    """Displays menu and prompts for user input"""
+    MENU = "C - Convert Celsius to Fahrenheit \nF - Convert Fahrenheit to Celsius \nQ - Quit"
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+main()
