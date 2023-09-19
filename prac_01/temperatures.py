a891d1c1cacfa59abaaba0a746d5e71eb3f0e3ae
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-19 14:41:37 +1000

    reverted changes, was using this for prac2

--- prac_01/temperatures.py
+++ prac_01/temperatures.py
@@ -1,33 +1,27 @@
-def main():
-    """Displays menu and prompts for user input"""
-    MENU = "C - Convert Celsius to Fahrenheit \nF - Convert Fahrenheit to Celsius \nQ - Quit"
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print(f"Result: {fahrenheit:.2f} F")
+    elif choice == "F":
+        # TODO: Write this section to convert F to C and display the result
+        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+        # Remove the "pass" statement when you are done. It's a placeholder.
+        farenheit = float(input("Farenheit: "))
+        celsius = 5 / 9.0 * (farenheit - 32)
+        print(f"Result: {celsius:.2f} C")
+    else:
+        print("Invalid option")
     print(MENU)
     choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = celsius_to_fahrenheit(celsius)
-            print(f"Result: {fahrenheit:.2f} F")
-        elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = fahrenheit_to_celsius(fahrenheit)
-            print(f"Result: {celsius:.2f} C")
-        else:
-            print("Invalid option")
-        print(MENU)
-        choice = input(">>> ").upper()
-    print("Thank you.")
-
-
-def fahrenheit_to_celsius(farenheit):
-    """converts fahrenheit to celsius"""
-    celsius = 5 / 9.0 * (farenheit - 32)
-    return celsius
-
-
-def celsius_to_fahrenheit(celsius):
-    """convert celsius to fahrenheit"""
-    fahrenheit = celsius * 9.0 / 5 + 32
-    return fahrenheit
-
-main()
+print("Thank you.")
