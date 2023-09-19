378024121eca71358364e32427cc41f4c2bf532b
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-19 15:47:59 +1000

    Added random score generator

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,10 +1,19 @@
+from random import *
+
+
 def main():
+    """Grabs score from user input and prints remark"""
     score = float(input("Enter score: "))
+    remark = get_remark(score)
+    print(remark)
+    score = randint(0, 100)
+    print(f"Random score: {score}")
     remark = get_remark(score)
     print(remark)
 
 
 def get_remark(score):
+    """Evaluates score and allocates corresponding remark string"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score >= 90:
