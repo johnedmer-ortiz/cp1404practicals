f33fc11305129978a6fd85275b023072426ae77c
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-19 15:20:57 +1000

    separated printing and remarks into functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,15 +1,18 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+def main():
+    score = float(input("Enter score: "))
+    remark = get_remark(score)
+    print(remark)
 
-score = float(input("Enter score: "))
-    
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def get_remark(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
