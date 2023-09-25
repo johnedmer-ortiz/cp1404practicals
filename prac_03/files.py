0dff53154d89723a71e6cd6d682b22fde3b021d3
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-26 09:35:14 +1000

    changed file object name to in_file for task 2,3 and 4

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,21 +7,21 @@ out_file = open("name.txt", "w")
 print(f"{name}", file=out_file)
 out_file.close()
 
-out_file = open("name.txt", "r")
-print(f"Your name is {out_file.readline()}")
-out_file.close()
+in_file = open("name.txt", "r")
+print(f"Your name is {in_file.readline()}")
+in_file.close()
 
-out_file = open("numbers.txt", "r")
+in_file = open("numbers.txt", "r")
 #print(f"{int(out_file.readline()) + int(out_file.readline())}")
 result = 0
 for i in range(2):
-    result = result + int(out_file.readline())
+    result = result + int(in_file.readline())
 print(result)
-out_file.close()
+in_file.close()
 
-out_file = open("numbers.txt", "r")
+in_file = open("numbers.txt", "r")
 result = 0
-for line in out_file:
+for line in in_file:
     result = int(line) + result
 print(result)
-out_file.close()
+in_file.close()
