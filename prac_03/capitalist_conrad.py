04ccb08b9ec4b1103debb50b34678d16361ef646
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-26 07:41:25 +1000

    Initial commit. Added number_of_days feature

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -16,9 +16,12 @@ MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+number_of_days = 1
+
+print(f"On day {number_of_days} price is ${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
+    number_of_days = number_of_days + 1
     price_change = 0
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
@@ -32,4 +35,4 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is ${price:,.2f}")
