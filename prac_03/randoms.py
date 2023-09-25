a6a455101ed153609fa31b714af2a1a944f83be5
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-25 22:11:47 +1000

    Initial commit, answered questions for line 1 and 2

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+
+# print(random.randint(5, 20))
+# What did you see on line 1? it printed random numbers from 5 to 20
+# What was the smallest number you could have seen? smallest is 5
+# what was the largest? largest is 20
+
+# print(random.randrange(3, 10, 2))
+# What did you see on line 2? it printed values 3, 5, 7 and 9
+# What was the smallest number you could have seen? smallest is 3
+# what was the largest? largest is 9
+# Could line 2 have produced a 4? no, the code has a step distance of 2, the number 4 is skipped
+
