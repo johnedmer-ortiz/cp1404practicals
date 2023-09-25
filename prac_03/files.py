fbf03c3fd7f59960efd7091d528cddc053b6cca0
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-26 09:22:32 +1000

    Added code for task 3. Note that theres a commented line, the line does the same thing but less versatile.

--- prac_03/files.py
+++ prac_03/files.py
@@ -10,3 +10,11 @@ out_file.close()
 out_file = open("name.txt", "r")
 print(f"Your name is {out_file.readline()}")
 out_file.close()
+
+out_file = open("numbers.txt", "r")
+#print(f"{int(out_file.readline()) + int(out_file.readline())}")
+result = 0
+for i in range(2):
+    result = result + int(out_file.readline())
+print(result)
+out_file.close()
