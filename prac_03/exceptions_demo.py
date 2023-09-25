5887034fd99a0a359e582b19be936192a92ad190
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-26 08:29:16 +1000

    Answered question 3 and modified code. Added if block for zero denominator handling, and commented out  ZeroDivisionError except block

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,13 +9,17 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
+    if denominator == 0:
+        print("You cannot divide by zero!")
+    else:
+        fraction = numerator / denominator
+        print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
+# except ZeroDivisionError:
+#     print("Cannot divide by zero!")
 print("Finished.")
 
 # 1. ValueError occurs when either of the two inputs are not valid numbers
-# 2. ZeroDivisionError occurs when the denominator input is zero
+# 2. ZeroDivisionError occurs when zero denominator will avoid the possibility of ZeroDivisionError
+# 3. An if block that checks and handles zero denominators will eliminate the need for the ZeroDivisionError except block
