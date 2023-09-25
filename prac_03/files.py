da673dbc4998cfa66132e857b12ab2a61b590d76
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-26 08:59:20 +1000

    fixed file name string variable, as the variable is the as the open output file

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,7 +3,7 @@ CP1404 - prac_03 - files
 """
 
 name = input("Enter name: ")
-out_file = name + ".txt"
-out_file = open(out_file, "w")
+file_name = name + ".txt"
+out_file = open(file_name, "w")
 print(f"{name}", file=out_file)
 out_file.close()
