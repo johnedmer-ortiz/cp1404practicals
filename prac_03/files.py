66e0074458c0b445cc30d14e53ecdaa9c8c18f15
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-09-26 09:08:14 +1000

    Added file read code for task 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,3 +7,6 @@ out_file = open("name.txt", "w")
 print(f"{name}", file=out_file)
 out_file.close()
 
+out_file = open("name.txt", "r")
+print(f"Your name is {out_file.readline()}")
+out_file.close()
