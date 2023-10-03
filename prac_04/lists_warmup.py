23a648d70fe5b5747e22700b2d59767e94172a9b
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-04 08:09:17 +1000

    fixed comment spelling error

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -4,7 +4,7 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # numbers[-1]          2
 # numbers[3]           1
 # numbers[:-1]         3, 1, 4, 1, 5, 9
-# numbers[3:4]         1  ..... note that this is stil a list
+# numbers[3:4]         1  ..... note that this is still a list
 # 5 in numbers         True
 # 7 in numbers         False
 # numbers + [6,5,3]    3, 1, 4, 1, 5, 9, 2, 6, 5, 3
