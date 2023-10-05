599dce0cd152ea07045918a6478c45271583a5db
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-05 12:44:02 +1000

    rebuilt for loop to account for length and repetition checking

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,5 +4,9 @@ quick_picks = []
 number_of_picks = int(input("How many quick picks? "))
 
 for i in range(number_of_picks):
-    quick_picks.append(tuple(random.randint(1, 45) for number in range(6)))
-    print(str(quick_picks[i]).strip("()"))
+    random_numbers = [random.randint(1, 45) for i in range(6)]
+    unique_numbers = []
+    while len(unique_numbers) != 6:
+        unique_numbers = [number for number in random_numbers if number not in unique_numbers]
+
+    #print(str(quick_picks[i]).strip("()"))
