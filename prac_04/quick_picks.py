860892c2d958874381cce946604652e213fbb4d5
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-05 12:57:03 +1000

    Made logic correction by moving random number generator inside the while loop

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,9 +4,11 @@ quick_picks = []
 number_of_picks = int(input("How many quick picks? "))
 
 for i in range(number_of_picks):
-    random_numbers = [random.randint(1, 45) for i in range(6)]
     unique_numbers = []
     while len(unique_numbers) != 6:
+        random_numbers = [random.randint(1, 45) for j in range(6)]
         unique_numbers = [number for number in random_numbers if number not in unique_numbers]
+    quick_picks.append(sorted(unique_numbers))
+
 
     #print(str(quick_picks[i]).strip("()"))
