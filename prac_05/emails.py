5c6e2c0c447de761b8858bdb59e400664c7c2c1b
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-09 08:15:59 +1000

    Added print function to display emails and names

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,15 +5,22 @@ def main():
     while email != "":
         email_to_name[email] = extract_name(email)
         email = input("Email: ")
+    display_emails(email_to_name)
 
 
 def extract_name(email):
     name = email.split("@")[0]
     check_name = input(f"Is your name {name}? (Y/N)").upper()
-    if check_name == "" or check_name == "Y" or "YES":
+    if check_name == "" or check_name == "Y" or check_name == "YES":
         return name
     else:
         return input("Name:")
+
+
+def display_emails(email_to_name):
+    print("")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
 main()
