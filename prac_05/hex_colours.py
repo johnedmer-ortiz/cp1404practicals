a0ec6c8c75ae00d90e3466e8833560082a834c72
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-08 20:07:36 +1000

    Created colour to hex dictionary.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+colour_to_hex = {"Army Green": "#4b5320", "Arylide Yellow": "#e9d66b", "Ash Grey": "#b2beb5",
+                 "Asparagus": "#87a96b", "Aureolin": "#fdee00", "Baby Blue": "#89cff0",
+                 "Baker-Miller Pink": "#ff91af", "Banana Mania": "#fae7b5", "Barn Red": "#7c0a02",
+                 "Battleship Gray": "#848482"}
+
