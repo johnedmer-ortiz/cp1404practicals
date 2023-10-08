96b234b1208c800d17f7a7c1bc4f79008cc9ad60
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-09 09:09:33 +1000

    Constructed dictionary for the champions and their number of wins

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,5 +7,12 @@ with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
     del games[0]
 
     for game in games:
-        if game[2] not in  champions:
+        if game[2] not in champions:
             champions.append(game[2])
+
+    win = [0 for champion in range(len(champions))]
+    champion_to_win= dict(zip(champions, win))
+    for game in games:
+        if game[2] in champions:
+            champion_to_win[game[2]] += 1
+
