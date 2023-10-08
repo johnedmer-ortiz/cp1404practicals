0adfb9dfef717ab938246b1162356435a1e4b044
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-09 09:44:00 +1000

    Added docstring and function comments

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,4 +1,10 @@
+"""
+CP1404 - prac_05 - Wimbledon Games
+"""
+
+
 def main():
+    """Main function for printing to display"""
     games = create_games_list()
     champions, countries = create_winners_list(games)
     countries = sorted(countries)
@@ -14,7 +20,9 @@ def main():
         else:
             print(country)
 
+
 def create_games_list():
+    """Parses CSV file and creates a games list"""
     games = []
     with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
         for game in in_file:
@@ -24,6 +32,7 @@ def create_games_list():
 
 
 def create_winners_list(games):
+    """Creates a non-repeating lists of winning champions and countries"""
     countries = []
     champions = []
     for game in games:
@@ -35,6 +44,7 @@ def create_winners_list(games):
 
 
 def count_scores(champions, games):
+    """Counts number of wins of champions"""
     win = [0 for champion in range(len(champions))]
     champion_to_win = dict(zip(champions, win))
     for game in games:
