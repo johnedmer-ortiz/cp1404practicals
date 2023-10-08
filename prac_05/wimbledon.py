c4a98bd1dc3343aa8c13da9d947c32ce21bc8469
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-09 09:26:27 +1000

    Refactored loops into functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,20 +1,37 @@
-games = []
-champions = []
-countries = []
+def main():
+    games = create_games_list()
+    champions, countries = create_winners_list(games)
+    champion_to_win = count_scores(champions, games)
 
-with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
-    for game in in_file:
-        games.append(game.split(","))
-    del games[0]
 
+def create_games_list():
+    games = []
+    with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
+        for game in in_file:
+            games.append(game.split(","))
+        del games[0]
+    return games
+
+
+def create_winners_list(games):
+    countries = []
+    champions = []
     for game in games:
         if game[2] not in champions:
             champions.append(game[2])
             if game[1] not in countries:
                 countries.append(game[1])
+    return champions, countries
 
+
+def count_scores(champions, games):
+    champion_to_win = {}
     win = [0 for champion in range(len(champions))]
     champion_to_win = dict(zip(champions, win))
     for game in games:
         if game[2] in champions:
             champion_to_win[game[2]] += 1
+    return champion_to_win
+
+
+main()
