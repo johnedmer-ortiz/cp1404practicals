0a348b93b6fc480e82d6edac57e049a588f3e2c4
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-09 07:33:47 +1000

    Added extract_name method. Added another email input in main method

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,5 +2,11 @@ def main():
     """Main function"""
     email = input("Email: ")
     while email != "":
-        extract_name()
+        extract_name(email)
+        email = input("Email: ")
 
+def extract_name(email):
+    name = email.split("@")[0]
+    check_name = input(f"Is your name {name}? (Y/N)")
+
+main()
