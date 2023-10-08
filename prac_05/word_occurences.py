24e8b878160fc760582dd9d3a47f75de86abffed
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-08 21:09:42 +1000

    Added string input processing loop to create a unique list of words

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -3,3 +3,10 @@ Word Occurences
 Estimate: 60 minutes
 Actual:
 """
+
+phrase = input("Enter a string: ").split()
+words = []
+for word in phrase:
+    if word in phrase and word not in words:
+       words.append(word)
+
