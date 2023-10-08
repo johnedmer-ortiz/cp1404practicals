313a7c62e1829783fcc8ceaf560877d1e9cb1d66
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-08 21:33:10 +1000

    deleted print(max_width)

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -12,7 +12,6 @@ for word in phrase:
         words.append(word)
 
 max_width = max([len(word) for word in words])
-print(max_width)
 
 counts = [0 for i in range(len(words))]
 word_to_count = dict(zip(sorted(words), counts))
