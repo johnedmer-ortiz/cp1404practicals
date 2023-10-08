ee12a353ecc50c490c8e877f1ec27351082141c3
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-09 08:56:51 +1000

    Modified first for loop to delete the first list element as this is just a header from the csv file. Also added another for loop to create a unique list of champions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,5 +1,11 @@
-lines = []
+games = []
+champions = []
 
 with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
-    for line in in_file:
-        lines.append(line.split(","))
+    for game in in_file:
+        games.append(game.split(","))
+    del games[0]
+
+    for game in games:
+        if game[2] not in  champions:
+            champions.append(game[2])
