20e534ac8a0fd1081268b28e6bfa00f4478899da
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-09 09:14:14 +1000

    Modified 2nd for loop to construct the winning countries list too

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,5 +1,6 @@
 games = []
 champions = []
+countries = []
 
 with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
     for game in in_file:
@@ -9,10 +10,11 @@ with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
     for game in games:
         if game[2] not in champions:
             champions.append(game[2])
+            if game[1] not in countries:
+                countries.append(game[1])
 
     win = [0 for champion in range(len(champions))]
-    champion_to_win= dict(zip(champions, win))
+    champion_to_win = dict(zip(champions, win))
     for game in games:
         if game[2] in champions:
             champion_to_win[game[2]] += 1
-
