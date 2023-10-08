c3aa7a517fa400f35182c02bcfb80afeb1f56395
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-08 21:14:49 +1000

    Calculated the width of the longest word

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,12 +1,15 @@
 """
-Word Occurences
+Word Occurrences
 Estimate: 60 minutes
 Actual:
 """
 
 phrase = input("Enter a string: ").split()
+
 words = []
 for word in phrase:
     if word in phrase and word not in words:
-       words.append(word)
+        words.append(word)
 
+max_width = max([len(word) for word in words])
+print(max_width)
