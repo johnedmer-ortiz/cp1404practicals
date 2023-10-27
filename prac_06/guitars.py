5f74b7c00e5533b1562aface7b337058d37ff8b0
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-28 09:55:51 +1000

    Added initial input fields

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -3,3 +3,14 @@ CP1404 - prac_06 guitars
 """
 
 from guitar import Guitar
+
+print("My guitars!")
+
+guitars = []
+
+name = input("Name: ")
+year = int(input("Year: "))
+cost = float(input("Cost: $"))
+
+guitars.append(Guitar(name, year, cost))
+
