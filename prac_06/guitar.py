cf83b518de34c3efe2b21122975c82f2b5b7c1da
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-28 09:36:25 +1000

    added get_age() function

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,8 +4,10 @@ Estimated time to finish: 60 mins
 Actual:
 """
 
+
 class Guitars:
     """Represents a guitar"""
+
     def __init__(self, name="", year=0, cost=0):
         """Initialises guitar object"""
         self.name = name
@@ -13,4 +15,9 @@ class Guitars:
         self.cost = cost
 
     def __str__(self):
+        """Returns guitar details"""
         return f"{self.name} ({self.year}) : {self.cost}"
+
+    def get_age(self):
+        """Returns age of guitar relative to year 2023"""
+        return f"in 2023 the {self.name} is: 2023 - {self.year} = {2023 - self.year}"
