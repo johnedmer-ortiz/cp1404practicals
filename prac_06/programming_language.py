26237d9e2eacec1d2f131077640adb7487a81f68
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-28 09:04:09 +1000

    Added __init__ function to class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,3 +3,13 @@ CP1404 - prac_06 programming languages
 Estimated time to finish: 40 mins
 Actual:
 """
+
+Class ProgrammingLanguage:
+    """Represents a programming language"""
+
+    def __init__(self, language, typing, reflection, year):
+        """Initialises language instance"""
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
