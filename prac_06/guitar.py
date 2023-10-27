3fc295c83e841816e634d42b7d3507b36f8ef33b
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-28 09:31:11 +1000

    Added __init__ function

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,3 +3,11 @@ CP1404 - prac_06 guitars
 Estimated time to finish: 60 mins
 Actual:
 """
+
+class Guitars:
+    """Represents a guitar"""
+    def __init__(self, name="", year=0, cost=0):
+        """Initialises guitar object"""
+        self.name = name
+        self.year = year
+        self.cost = cost
