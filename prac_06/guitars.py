dad3d026d60e2f9ae3c3f8356fad3d5d036f92b8
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-10-28 10:11:22 +1000

    Added loop to display details of all guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -22,4 +22,6 @@ while name != "":
         cost = float(input("Cost: $"))
         guitars.append(Guitar(name, year, cost))
 
-
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = " (vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
