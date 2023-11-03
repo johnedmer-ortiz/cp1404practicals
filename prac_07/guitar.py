6703bc22ca3f5b8188c68cd8e8a0bdf522687b19
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-04 09:37:31 +1000

    Initial commit. Added guitar.py and guitars.csv files

---
+++ prac_07/guitar.py
@@ -0,0 +1,29 @@
+"""
+CP1404 - prac_06 guitars
+Estimated time to finish: 60 mins
+Actual: 42mins
+"""
+
+
+class Guitar:
+    """Represents a guitar"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialises guitar object"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Returns guitar details"""
+        return f"{self.name} ({self.year}) : {self.cost}"
+
+    def get_age(self):
+        """Returns age of guitar relative to year 2022"""
+        return 2022 - self.year
+
+    def is_vintage(self):
+        """Checks guitar if it's vintage or not"""
+        if self.get_age() >= 50:
+            return True
+        return False
