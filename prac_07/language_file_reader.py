f67b3c5f51891909261fb3d76a94cda32289db8e
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-04 09:31:43 +1000

    Modified language_file_reader.py to include pointer arithmetic field

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -26,9 +26,11 @@ def main():
         # print(parts)  # debugging
         # Reflection is stored as a string (Yes/No) and we want a Boolean
         reflection = parts[2] == "Yes"
+        # Pointer arithmetic is stored as a string (Yes/No), need to convert to boolean
+        pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
@@ -63,7 +65,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer_arithmetic')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
