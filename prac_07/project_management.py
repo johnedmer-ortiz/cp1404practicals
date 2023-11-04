758ce8adaf5ff9240e52d917cc0693a38a2935cb
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-04 12:33:11 +1000

    Added display_menu function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,3 +1,26 @@
 """
 CP1404 - prac_07_feedback - project_management.py
+Estimated finish time: 2 hours
+Actual finish time:
 """
+
+from project import Project
+
+
+def main():
+    """Main function. Program starting point"""
+    display_menu()
+
+
+def display_menu():
+    """Displays menu"""
+    print("- (L)oad projects")
+    print("- (S)ave projects")
+    print("- (D)isplay projects")
+    print("- (F)ilter projects by date")
+    print("- (A)dd new project")
+    print("- (U)pdate project")
+    print("- (Q)uit")
+
+
+main()
