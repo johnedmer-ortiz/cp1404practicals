5bfcdc8feb583462e845e11baab50407efe34090
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-04 12:20:53 +1000

    Added class Project and __init__ method

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,3 +1,14 @@
 """
 CP1404 - prac_07_feedback - project.py
 """
+
+
+class Project:
+    """Represents a project"""
+
+    def __init__(self, name, start_date, priority, cost, completion_percent):
+        self.name = name
+        self.star_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion_percent = completion_percent
