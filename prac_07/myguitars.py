6151302cc764a32a905529d2b8ba40b17c54aa9a
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-04 10:06:07 +1000

    Added read_guitars function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,3 +1,24 @@
 """
 CP1404 - prac_07_feedback - myguitars.py
 """
+
+from guitar import Guitar
+
+FILE_NAME = "guitars.csv"
+
+
+def main():
+    """Main function. Program starting point"""
+    my_guitars = read_guitars()
+
+
+def read_guitars():
+    """Reads csv file into objects"""
+    my_guitars = []
+    with open(FILE_NAME, "r") as in_file:
+        for line in in_file:
+            my_guitars.append(Guitar(line[0], line[1], line[2]))
+    return my_guitars
+
+
+main()
