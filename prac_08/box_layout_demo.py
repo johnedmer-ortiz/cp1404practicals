9de964f580cdf6d20c7423d6ee1e406d2be1a689
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-12 09:25:54 +1000

    Added clear button event handler

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
@@ -12,5 +13,8 @@ class BoxLayoutDemo(App):
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
+    def handle_clear(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 BoxLayoutDemo().run()
