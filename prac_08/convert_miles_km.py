83b14d1d9b6912ae15a00161a56186204ec2b476
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-12 11:31:51 +1000

    Added comments and function docstrings

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -10,20 +10,24 @@ CONV_FACTOR = 1.6
 
 
 class DistanceConverter(App):
+    """Represents a GUI App object"""
     output_distance = StringProperty()
 
     def build(self):
+        """Sets title and constructs GUI layout at instantiation"""
         self.title = "Miles to Kilometres Converter"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
     def handle_calculate(self):
+        """Converts miles to kilometres"""
         try:  # sets output to 0.0 when calc button is pressed and invalid input is entered
             self.output_distance = f"{float(self.root.ids.miles_input.text) * CONV_FACTOR:.2f}"
         except ValueError:
             self.output_distance = "0.0"
 
     def handle_increment(self, increment):
+        """Increments or decrements miles by 1"""
         try:  # try-except block to handle increments when input is invalid
             if increment == 1:
                 if self.root.ids.miles_input.text == "":  # checks TextInput field if empty
