a59f8cf72158c5644843ba8321b19f6294a7ffb2
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-12 10:27:27 +1000

    Created basic box layout as per prac requirements

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,3 +1,14 @@
 """
 CP1404 - prac_08 - miles to kilometres converter
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+class DistanceConverter(App):
+    def build(self):
+        self.title = "Miles to Kilometres Converter"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+DistanceConverter().run()
