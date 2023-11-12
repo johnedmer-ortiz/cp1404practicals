366ce8cc2a9dc673b998556ca460ed4fe6d4ae41
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2023-11-12 10:57:23 +1000

    Added handle calculate function to change output label.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,11 +4,21 @@ CP1404 - prac_08 - miles to kilometres converter
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+CONV_FACTOR = 1.6
+
 
 class DistanceConverter(App):
+    output_distance = StringProperty()
+
     def build(self):
         self.title = "Miles to Kilometres Converter"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
+
+    def handle_calculate(self):
+        self.output_distance = f"{float(self.root.ids.miles_input.text) * CONV_FACTOR}"
+
 
 DistanceConverter().run()
