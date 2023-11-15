552da5db573edbee15d2f5fc020595b05a4dcbba
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 09:59:32 +1000

    Override drive method

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -2,6 +2,8 @@
 CP1404 - prac_09 unreliable_car
 """
 
+import random
+
 from car import Car
 
 
@@ -9,3 +11,13 @@ class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        if random.randint(0, 100) < self.reliability:
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self._odometer += distance
+            return distance
