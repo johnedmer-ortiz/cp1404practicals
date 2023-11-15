751a87fd2f77cd76cda8daf66a15142da4c5e57f
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 09:21:06 +1000

    Add comments and function docstrings

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -6,7 +6,11 @@ from taxi import Taxi
 
 
 def main():
+    """Main function for testing Taxi subclass"""
     my_taxi = Taxi("Prius 1", 100, 1.23)
+    # initialises the first two parameters using Car class
+    # initialises the third parameter using taxi subclass
+
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.current_fare_distance = 0
