6997f981c11f21da6968e1d52aa40011de2efe9b
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 09:54:37 +1000

    Create UnreliableCar class and inherit from Car

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,3 +1,11 @@
 """
 CP1404 - prac_09 unreliable_car
 """
+
+from car import Car
+
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
