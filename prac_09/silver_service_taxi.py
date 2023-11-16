6105d46831cf2a19a13a6748adca56334ca285e3
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 10:36:54 +1000

    Override get_fare method to include flagfall price

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -10,3 +10,6 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.price_per_km = fanciness * Taxi.price_per_km
         self.flagfall = flagfall
+
+    def get_fare(self):
+        return (self.price_per_km * self.current_fare_distance) + self.flagfall
