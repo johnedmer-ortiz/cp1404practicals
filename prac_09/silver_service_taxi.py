a813d4c149cb04a287f25d08b780be926aa027c0
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 10:48:15 +1000

    Add main function to test SilverServiceTaxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,14 +6,14 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
 
-    def __init__(self, name, fuel, fanciness, flagfall):
+    def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.price_per_km = fanciness * Taxi.price_per_km
-        self.flagfall = flagfall
+        self.flagfall = 4.50
 
     def get_fare(self):
         return (self.price_per_km * self.current_fare_distance) + self.flagfall
 
     def __str__(self):
         return (f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km "
-                f"plus flagfall of ${self.flagfall}")
+                f"plus flagfall of ${self.flagfall:.2f}")
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,3 +1,16 @@
 """
 CP1404 - prac09 - silver_service_taxi_test
 """
+
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    test_taxi = SilverServiceTaxi("Limo", 150, 2)
+    print(test_taxi)
+    test_taxi.drive(18)
+    print(test_taxi)
+    print(f"Fare after drive: {test_taxi.get_fare()}")
+
+
+main()
