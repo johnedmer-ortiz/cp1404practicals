a4f869c0019282a5649aa50ed72d4f9387d5720c
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 22:27:01 +1000

    Add create_taxis() method to instantiate taxi selection

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,3 +1,18 @@
 """
 CP1404 - prac09 - taxi_simulator
 """
+
+from silver_service_taxi import SilverServiceTaxi
+from taxi import Taxi
+
+
+def main():
+    taxis = create_taxis()
+
+
+def create_taxis():
+    taxis = [Taxi("Commodore", 100), SilverServiceTaxi("Mustang", 100, 5), SilverServiceTaxi("Corvette", 200, 8)]
+    return taxis
+
+
+main()
