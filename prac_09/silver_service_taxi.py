122381f8d6fc76356e14c6baedf79ed3a8be636f
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 10:30:01 +1000

    Add override __init__ method for SilverServiceTaxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,3 +1,12 @@
 """
 CP1404 - prac09 - silver_service_taxi
 """
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+
+    def __init__(self, name, fuel, fanciness, flagfall):
+        super().__init__(name, fuel)
+        self.price_per_km = fanciness * Taxi.price_per_km
+        self.flagfall = flagfall
