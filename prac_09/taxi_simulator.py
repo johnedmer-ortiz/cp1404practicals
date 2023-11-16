7d89951257dfe23c2ad11042d5d2585c1b6b2863
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-17 07:16:56 +1000

    Add drive() method in taxi_simulator.py

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -13,16 +13,19 @@ def main():
     print("Let's drive!")
     taxis = create_taxis()
     taxi_choice = ""
+    fare = 0
     customer_choice = input(TAXI_CHOICES).upper()
     while customer_choice != "Q":
         if customer_choice == "C":
             taxi_choice = select_taxi(taxis)
-        print(f"Bill to date: ${calculate_bill(taxis[taxi_choice]):.2f}")
+        if customer_choice == "D":
+            fare += drive(taxis[taxi_choice])
+        print(f"Bill to date: ${fare:.2f}")
         customer_choice = input(TAXI_CHOICES).upper()
 
 
 def create_taxis():
-    taxis = [Taxi("Commodore", 100), SilverServiceTaxi("Mustang", 100, 5), SilverServiceTaxi("Corvette", 200, 8)]
+    taxis = [Taxi("Commodore", 100), SilverServiceTaxi("Mustang", 100, 2), SilverServiceTaxi("Corvette", 200, 3)]
     return taxis
 
 
@@ -33,6 +36,13 @@ def select_taxi(taxis):
 
 
 def calculate_bill(taxi):
+    return taxi.get_fare()
+
+
+def drive(taxi):
+    distance = int(input("Drive how far? "))
+    taxi.drive(distance)
+    print(f"Your {taxi.name} trip cost you ${taxi.get_fare():.2f}")
     return taxi.get_fare()
 
 
