642fef29cfcdb019ad7422e10384ffbff63df72d
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 22:37:42 +1000

    Add display_taxis() method to display taxi choices and corresponding details of the rides

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,14 +5,28 @@ CP1404 - prac09 - taxi_simulator
 from silver_service_taxi import SilverServiceTaxi
 from taxi import Taxi
 
+TAXI_CHOICES = ("q)uit, c)hoose taxi, d)rive"
+                ">>> ")
+
 
 def main():
+    print("Let's drive!")
     taxis = create_taxis()
+    customer_choice = input(TAXI_CHOICES).upper()
+    while customer_choice != "Q":
+        if customer_choice == "C":
+            display_taxis(taxis)
+        customer_choice = input(TAXI_CHOICES).upper()
 
 
 def create_taxis():
     taxis = [Taxi("Commodore", 100), SilverServiceTaxi("Mustang", 100, 5), SilverServiceTaxi("Corvette", 200, 8)]
     return taxis
+
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
 
 
 main()
