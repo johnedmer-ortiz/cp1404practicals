b3094290af2b246ba49672a8ef230e2662f3201c
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 22:57:32 +1000

    Add calculate_bill() method

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,17 +5,19 @@ CP1404 - prac09 - taxi_simulator
 from silver_service_taxi import SilverServiceTaxi
 from taxi import Taxi
 
-TAXI_CHOICES = ("q)uit, c)hoose taxi, d)rive"
+TAXI_CHOICES = ("q)uit, c)hoose taxi, d)rive\n"
                 ">>> ")
 
 
 def main():
     print("Let's drive!")
     taxis = create_taxis()
+    taxi_choice = ""
     customer_choice = input(TAXI_CHOICES).upper()
     while customer_choice != "Q":
         if customer_choice == "C":
-            display_taxis(taxis)
+            taxi_choice = select_taxi(taxis)
+        print(f"Bill to date: ${calculate_bill(taxis[taxi_choice]):.2f}")
         customer_choice = input(TAXI_CHOICES).upper()
 
 
@@ -27,6 +29,11 @@ def create_taxis():
 def select_taxi(taxis):
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+    return int(input("Choose taxi: "))
+
+
+def calculate_bill(taxi):
+    return taxi.get_fare()
 
 
 main()
