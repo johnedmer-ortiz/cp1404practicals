9ec014ae1396e1123c0d2d50f8863de9ae63dfc0
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-16 19:32:17 +1000

    Add function docstrings

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,21 +3,26 @@ CP1404 - prac_09 band
 """
 
 
-class Band():
+class Band:
+    """Represents a music band"""
 
     def __init__(self, band_name):
+        """Initialises band name and empty musicians list"""
         self.band_name = band_name
         self.musicians = []
 
     def add(self, musician):
+        """Adds a musician"""
         self.musicians.append(musician)
 
     def __str__(self):
+        """Returns band details as a string"""
         band = []
         for musician in self.musicians:
             band.append(f"{musician}")
         return f"{self.band_name} ({','.join(band)})"
 
     def play(self):
+        """Calls play() method for each musician object"""
         for musician in self.musicians:
             print(musician.play())
