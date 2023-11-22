9ac95c046327f1d354ddafa6382076cdc7b234f2
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-22 20:41:38 +1000

    Create basic skeleton of wiki search loop

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,3 +1,18 @@
 """
 CP1404 - prac_10 - wiki
 """
+
+import wikipedia
+
+
+def main():
+    query = input("Enter search string: ")
+    suggestion = wikipedia.suggest(query)
+    print(wikipedia.summary(suggestion))
+    while query != "":
+        query = input("Enter search string: ")
+        suggestion = wikipedia.suggest(query)
+        print(wikipedia.summary(suggestion))
+
+
+main()
