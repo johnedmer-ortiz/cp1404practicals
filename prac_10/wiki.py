ed6102e141ae05d9034ab1bac9d3a0962d70d9a5
johnedmer-ortiz <johnedmer.ortiz@my.jcu.edu.au>
2023-11-22 20:59:51 +1000

    Implement ValueError and DisambiguationError handling for when suggestion returns 'None' and page cannot be found.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -7,9 +7,14 @@ import wikipedia
 
 def main():
     query = None
+    query = input("Enter search string: ")
     while query != "":
+        try:
+            suggestion = wikipedia.suggest(query)
+            print(wikipedia.summary(suggestion))
+        except ValueError:
+            print("Wikipedia suggestion returned 'None'. Try a different search query.")
+        except wikipedia.DisambiguationError:
+            print("Cannot find page. Try again.")
         query = input("Enter search string: ")
-        suggestion = wikipedia.suggest(query)
-        print(wikipedia.summary(suggestion))
-
 main()
